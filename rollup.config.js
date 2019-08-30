815e225a5e2731bb38ec250e0d9a27547fdb37e5
Alexander Kutterer <alexander.kutterer@gmail.com>
2019-08-30 12:48:56 +0200

    clean up

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-// import replace from 'rollup-plugin-replace';
 import vue from 'rollup-plugin-vue'
-// import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
 
 const globals = { vue: 'Vue' };
-// const isProduction = !process.env.ROLLUP_WATCH;
 const external = Object.keys(pkg.dependencies);
 
 export default {
@@ -12,19 +9,15 @@ export default {
     output: {
         file: './build/bundle.js',
         format: 'iife',
-        // globals,
-        globals: {vue: 'Vue'}
+        globals,
     },
-    external: ['vue'],
-    // external,
+    external,
     plugins: [
-        // resolve(),
         vue({
             // template: {
-            //     isProduction,
-            //     compilerOptions: {preserveWhitespace: false}
+            //     isProduction: true,
+            //     compilerOptions: {preserveWhitespace: false} //deprecated?
             // },
-            // css: true
         }),
     ]
 };
