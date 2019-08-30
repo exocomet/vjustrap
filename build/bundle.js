bd4f52a9e24fccf304c1ef450f2279d944ed50cd
Alexander Kutterer <alexander.kutterer@gmail.com>
2019-08-30 02:12:11 +0200

    latest build

--- build/bundle.js
+++ build/bundle.js
@@ -3,21 +3,10 @@
 
     Vue = Vue && Vue.hasOwnProperty('default') ? Vue['default'] : Vue;
 
-    // Vue.component('foo-input', {
-    //     props: {
-    //         'x': Number
-    //     },
-    //     data: function() {
-    //         return {val: 0};
-    //     },
-    //     template: '<div>{{val}} <input v-model="val"></div>'
-    // });
-
     const operators = [
         {id: 'add', title: 'Add'},
         {id: 'subtract', title: 'Subtract'},
     ];
-
 
     var app = new Vue({
         el: '#app',
@@ -27,8 +16,6 @@
             numeric: 0,
             rangeMin: -100,
             range: 0,
-            // textarea,
-            // select,
             radioStuff: ['foddddo', 'badasdadaar'],
             selectedRadio: undefined,
             selectStuff: ['foo', 'bar'],
@@ -55,21 +42,10 @@
                 } else if (this.selectedOperator.id == 'subtract') {
                     this.subtract(value);
                 }
-            },
-            fetchData: function () {
-                // Taken from: https://vuejs.org/v2/examples/commits.html
-                // var xhr = new XMLHttpRequest()
-                // var self = this
-                // xhr.open('GET', apiURL + self.currentBranch)
-                // xhr.onload = function () {
-                //     self.commits = JSON.parse(xhr.responseText)
-                //     console.log(self.commits[0].html_url)
-                // }
-                // xhr.send()
             }
         },
         mounted: function() {
-            console.log('asd');
+            console.log("Look into my eyes you'll see who I am");
         },
         computed: {
             dataModel: function () {
